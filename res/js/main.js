aba93b13ee105fc4aa6c0b89034fa1864933f6cb
Scotchdopole <thonjirka@gmail.com>
2023-10-11 20:40:03 +0200

    updated

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,23 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 
+
+let scale = false;
 let numberOfCookies = 0;
 
 cookie.onclick = () => {
 
     numberOfCookies++;
-
     counter.innerText = "kráterů: " + numberOfCookies;
+
+    cookie.style.transform = "scale(0.8)";
+    
+    cookie.style.borderRadius = "40px";
+    setTimeout(function() {
+        cookie.style.transform = "scale(1.2)";
+        cookie.style.borderRadius = "25px";
+        
+      }, 80);
 }
+
+
